bf6bf17a602501073c4295b4ebcfb88c2552616b
ChaimW25 <willingerch@gmail.com>
2022-03-27 16:17:05 +0300

    update

--- manualMonitor.py
+++ manualMonitor.py
@@ -22,15 +22,17 @@ class manualMonitor:
 
     file1 = open(SERCIVE_LIST, 'r')
     lines1 = file1.readlines()
-
+    #reading the help file that helps us to save the dates in the "serviceList.log" file
     file2=open(DATES,'r')
     lines2 = file1.readlines()
-
+    #getting the times from the user as inputs
     now = datetime.now()
     currTime = now.strftime("%Y-%m-%d %H:%M:%S")
-    strtDate = input("Please enter start time in this format '22-03-25 12:35:14' : ")
-    endDate = input("Please enter end time in this format: '22-03-25 12:35:14' : ")
+    strtDate = input("Please enter start time in this format '2022-03-25 12:35:14' : ")
+    endDate = input("Please enter end time in this format: '2022-03-25 12:35:14' : ")
 
+    #using times[]-save the dates and their pos in the file,
+    # dateObj[]-  save the dates
     times=[]
     times.append("0")
     i=1
@@ -40,36 +42,41 @@ class manualMonitor:
         times.append(line.split('~'))
         dateObj.append(datetime.strptime(times[2*i-1], '%y-%m-%d %H:%M:%S'))
         i+=1
-
+    #convert the input daes to our format-not necessary
     strtDate=datetime.strptime(strtDate, '%y-%m-%d %H:%M:%S')
     endDate=datetime.strptime(endDate, '%y-%m-%d %H:%M:%S')
 
     i=1
-    #saving the first time start and end pos in the file, and the same withe the second time
+    #saving the user first time- start and end index pos range in the file
     for date in dateObj:
-        if strtDate<date:
+        if strtDate>date:
             strtTime=i*2
             strtTime2=(i+1)*2
             break
+        i+=1
+    # doing the same with the user second time
     for date in dateObj:
-        if endDate<date:
+        if endDate>date:
             endTime=i*2
             endTime2=(i+1)*2
             break
+        i+=1
 
+    #reading the file in the first time range and insert the sevices names into dict keys
     fileA=open(SERCIVE_LIST, 'r')
-    posA=fileA.seek(strtTime,strtTime)
+    posA=fileA.seek(times[strtTime],times[strtTime])
     for line in fileA:
-        while pos < strtTime2:
+        while pos < times[strtTime2]:
             serviceDict1[line] = "is running"
             pos = fileA.tell()
-
-    posA=fileA.seek(endTime,endTime)
+    #doing the same with the second time
+    posA=fileA.seek(times[endTime],times[endTime])
     for line in fileA:
-        while pos < endTime2:
+        while pos < times[endTime2]:
             serviceDict2[line] = "is running"
             pos = fileA.tell()
 
+    #print the changes between the first time and second time
     str=""
     for key in serviceDict1:
         if serviceDict2.get(key) == None:
